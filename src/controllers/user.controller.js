0a255c2319e6f58fea6cff9c45bba358af527cdd
Sirjan <jeetsirjan@gmail.com>
2025-06-27 01:25:12 +0530

    setup configured

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,11 @@
-import {asyncHandler} from "../utils/asynchandler.js";
+// controller is responsible for handling user-related requests
+// bridge between routes and services
+
+import asyncHandler from "../utils/asynchandler.js";
 
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
+    return res.status(200).json({
         success: true,
         message: "User registered successfully",
     });
