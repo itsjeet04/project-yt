54ed862d9541cd401263153d05f5a3331662add7
Sirjan <jeetsirjan@gmail.com>
2025-06-27 22:28:23 +0530

    user routes setup

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import registerUser from "../controllers/user.controller.js";
 const router = Router();
+// user routes
 
 router.route("/register").post(registerUser)
 
