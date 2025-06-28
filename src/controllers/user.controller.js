fd0146cf0acc378dfb7ccbc4fd3d2ab25d9f540d
Sirjan <jeetsirjan@gmail.com>
2025-06-28 21:02:20 +0530

    usercontroler - 1

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,10 +5,24 @@ import asyncHandler from "../utils/asynchandler.js";
 
 
 const registerUser = asyncHandler(async (req, res) => {
-    return res.status(200).json({
-        success: true,
-        message: "User registered successfully",
-    });
+//    get user data from frontend/postman
+//    validate data
+//    check if user already exists
+//    check for image , avtaar
+//    upload image to cloudinary
+//    create user object in database
+//    remove password and refresh token from response
+//    check if user is created successfully
+//    send response to frontend/postman
+      
+    const {username,fullName,email,avtar,coverImage,password} = req.body 
+    // destructuring data from request body
+    // form ,json data will be in req.body , url data will not be here but in req.query
+    
+    // testing 
+    console.log("User data received:", {username,fullName,email,password});    
+
+
 }) 
 
 export default registerUser;
