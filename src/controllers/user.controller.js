82f2717b2e24c381e10350a0d17878ecb0466397
sirjanjeet singh <jeetsirjan@gmail.com>
2025-07-07 22:36:50 +0530

    changeCurrentPassword

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -204,9 +204,13 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
   return res.status(200).json(new ApiResponse(200, {}, "Password changed successfully"));
   
 })
+
+
+
 export {
    registerUser,
    loginUser,
    logoutUser,
-   refreshAccessToken
+   refreshAccessToken,
+   changeCurrentPassword
 };        
