df4181274bcc42f82e50e4b87845e3d41208395c
sirjanjeet singh <jeetsirjan@gmail.com>
2025-07-10 16:18:57 +0530

    add  route to user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {loginUser, logoutUser, registerUser,refreshAccessToken} from "../controllers/user.controller.js";
+import {loginUser, logoutUser, registerUser,refreshAccessToken, changeCurrentPassword} from "../controllers/user.controller.js";
 import {upload} from "../middleware/multer.middleware.js";
 // import { verify } from "jsonwebtoken";
 import { verifyJWT } from "../middleware/auth.middleware.js";
@@ -24,5 +24,12 @@ router.route("/login").post(
 router.route("/logout").post(verifyJWT, logoutUser);
 // middleware to verify JWT token
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT,changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/history").get(verifyJWT, getWatchHistory)
 
 export default router;
